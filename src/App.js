20eec7ede3fcc1a7caf0e05aa32b7ebc7b4b315b
Jbanniere <bannier.julie91@gmail.com>
2023-01-16 12:54:13 +0000

    maj fontawesome

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx"
 import ErrorPage from "./components/ErrorPage.jsx"
 
+/* pour faire fonctionner fontawesome, rajouter:
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons"; => exemple pour ajouter le picto "trash"*/
+
+
 function App() {
   const [state, dispatch] = React.useContext(StoreContext)
   
